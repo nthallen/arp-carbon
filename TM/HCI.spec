6905516a25336a41806f4a57cd0681c1519c8cff
Norton Allen <allen@huarp.harvard.edu>
2012-04-19 15:57:03 +0000

    Initial versions

--- TM/HCI.spec
+++ TM/HCI.spec
@@ -4,7 +4,9 @@ tmcbase = digio.tmc
 tmcbase = idx64.tmc
 tmcbase = pwrmon.tmc
 tmcbase = qcli.tmc
+tmcbase = PTRH.tmc
 tmcbase = /usr/local/share/huarp/vl_temp.tmc
+tmcbase = /usr/local/share/huarp/tmdf.tmc
 
 cmdbase = /usr/local/share/huarp/root.cmd
 cmdbase = /usr/local/share/huarp/getcon.cmd
@@ -13,13 +15,19 @@ cmdbase = /usr/local/share/huarp/idx64.cmd
 cmdbase = idx64drv.cmd
 
 colbase = idx64col.tmc
+colbase = PTRH_col.tmc
 colbase = /usr/local/share/huarp/DACS_ID.tmc
 colbase = /usr/local/share/huarp/vl_temp_col.tmc
+colbase = /usr/local/share/huarp/tmdf_col.tmc
+
+swsbase = HCI.sws
 
 qclibase = QCLI_C.qcli QCLI_M.qcli QCLI_I.qcli
 
 SRC = pwrmon_conv.tmc idx64flag.tmc qcli_conv.tmc
+SRC = PTRH_conv.tmc
 SCRIPT = idx64.idx64
 
 HCIcol : -lsubbus
 HCIsrvr : -lsubbus
+HCIalgo : HCI.tma HCI.sws
--- TM/qclis.cmd
+++ TM/qclis.cmd
@@ -1,8 +1,8 @@
-%INTERFACE <SSP_C>
 %INTERFACE <SSP_M>
+%INTERFACE <SSP_C>
 %INTERFACE <SSP_I>
-%INTERFACE <QCLI_C>
 %INTERFACE <QCLI_M>
+%INTERFACE <QCLI_C>
 %INTERFACE <QCLI_I>
 
 %{
@@ -11,19 +11,19 @@
   #include "hsatod.h"
   #define QCLI_ICOS \
       (HSAD_OPT_A|HSAD_OPT_B|HSAD_OPT_C|HSAD_TRIG_3|HSAD_TRIG_RISING)
-  #define QCLI_C_ICOS QCLI_ICOS
   #define QCLI_M_ICOS QCLI_ICOS
+  #define QCLI_C_ICOS QCLI_ICOS
   #define QCLI_I_ICOS QCLI_ICOS
-  hsatod_setup_t SSP_C_setup;
   hsatod_setup_t SSP_M_setup;
+  hsatod_setup_t SSP_C_setup;
   hsatod_setup_t SSP_I_setup;
 
   static struct sspqcli_s {
     hsatod_setup_t *setup;
     cmdif_rd *if_ssp;
   } sspqcli_bd[3] = {
-    { &SSP_C_setup, &if_SSP_C },
     { &SSP_M_setup, &if_SSP_M },
+    { &SSP_C_setup, &if_SSP_C },
     { &SSP_I_setup, &if_SSP_I },
   };
 
@@ -32,14 +32,14 @@
 %}
 # &SSP returns an index into sspqcli_bd[]
 &SSP <int>
-  : SSP_C { $0 = 0; }
-  : SSP_M { $0 = 1; }
+  : SSP_M { $0 = 0; }
+  : SSP_C { $0 = 1; }
   : SSP_I { $0 = 2; }
   ;
 # &QCLI returns an inteface
 &QCLI <cmdif_rd *>
-  : QCLI_C { $0 = &if_QCLI_C; }
   : QCLI_M { $0 = &if_QCLI_M; }
+  : QCLI_C { $0 = &if_QCLI_C; }
   : QCLI_I { $0 = &if_QCLI_I; }
   ;
 &command
