ff1a785027b6e455a85d377d89303381bb07053f
Norton Allen <allen@huarp.harvard.edu>
2012-06-05 16:27:59 +0000

    new plots

--- eng/gHCIhkhk.m
+++ eng/gHCIhkhk.m
@@ -1,5 +1,5 @@
 function fig = gHCIhkhk(varargin);
 % gHCIhkhk(...)
 % HK_HK
-ffig = ne_group(varargin,'HK_HK','pHCIhkhkcpu','pHCIhkhkd','pHCIhkhksws');
+ffig = ne_group(varargin,'HK_HK','pHCIhkhkcpu','pHCIhkhkd','pHCIhkhkm','pHCIhkhksws');
 if nargout > 0 fig = ffig; end
