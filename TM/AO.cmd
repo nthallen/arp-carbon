f4c58f4e15111da1bf63eb681194bde4ad8cae23
Norton Allen <allen@huarp.harvard.edu>
2012-10-02 14:01:02 +0000

    Set pressure in Torr

--- TM/AO.cmd
+++ TM/AO.cmd
@@ -18,23 +18,26 @@
 %}
 &Command
   : &AO_volts %f (Enter 0-10 Volts) * { sbwr( $1, ao_scale($2, 1.0, 10.0) ); }
+  : &AO_pset %f (Enter 0-100 Torr) * { sbwr( $1, ao_scale($2, 1/30., 3.333) ); }
   ;
 &AO_volts <unsigned short>
-  : CO2 Set Cell Pressure Setpoint { $0 = CCelPSt_Address; }
   : CO2 Set Cell Temp Setpoint { $0 = CCelTSt_Address; }
   : CO2 Set Laser TEC Temp Setpoint { $0 = CLTETSt_Address; }
   : CO2 Set LPV Temp Setpoint { $0 = CLPVTSt_Address; }
   : GASDECK Set Gas Flow Setpoint { $0 = GsFlSt_Address; }
   : HK Set Sample Heater Temp Setpoint { $0 = SHTSt_Address; }
   : HK Set Space Heater Temp Setpoint { $0 = SpHtTSt_Address; }
-  : ISO Set Cell Pressure Setpoint { $0 = ICelPSt_Address; }
   : ISO Set Cell Temp Setpoint { $0 = ICelTSt_Address; }
   : ISO Set DPV Temp Setpoint { $0 = IDPVTSt_Address; }
   : ISO Set LPV External TEC Setpoint { $0 = ILPVTSt_Address; }
   : ISO Set Laser TEC Temp Setpoint { $0 = ILTETSt_Address; }
-  : MINI Set Cell Pressure Setpoint { $0 = MCelPSt_Address; }
   : MINI Set Cell Temp Setpoint { $0 = MCelTSt_Address; }
   : MINI Set DPV Temp Setpoint { $0 = MDPVTSt_Address; }
   : MINI Set Laser TEC Temp Setpoint { $0 = MLTETSt_Address; }
   : MINI Set LPV External TEC Setpoint { $0 = MLPVTSt_Address; }
+  ;
+&AO_pset <unsigned short>
+  : CO2 Set Cell Pressure Setpoint { $0 = CCelPSt_Address; }
+  : ISO Set Cell Pressure Setpoint { $0 = ICelPSt_Address; }
+  : MINI Set Cell Pressure Setpoint { $0 = MCelPSt_Address; }
   ;
