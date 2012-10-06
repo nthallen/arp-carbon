49bdecdefdadcec14063d7f0fd1b53ddf5389111
Norton Allen <allen@huarp.harvard.edu>
2012-10-06 20:11:47 +0000

    Eliminate HCI stuff

--- BAT/demo/eng/gHCIspan.m
+++
@@ -1,5 +0,0 @@
-function fig = gHCIspan(varargin);
-% gHCIspan(...)
-% SPAN
-ffig = ne_group(varargin,'SPAN','pHCIspannc','pHCIspann','pHCIspancpu');
-if nargout > 0 fig = ffig; end
--- BAT/demo/eng/ui_HCI.m
+++
@@ -1,9 +0,0 @@
-function ui_HCI;
-f = ne_dialg('Harvard Carbon Isotopes');
-f = ne_dialg(f, 'add', 0, 1, 'gHCIspan', 'SPAN' );
-f = ne_dialg(f, 'add', 1, 0, 'pHCIspannc', 'NC' );
-f = ne_dialg(f, 'add', 1, 0, 'pHCIspann', 'N' );
-f = ne_dialg(f, 'add', 1, 0, 'pHCIspancpu', 'CPU' );
-f = ne_listdirs(f, 'HCI_Data_Dir', 18);
-f = ne_dialg(f, 'newcol');
-f = ne_dialg(f, 'resize');
