a0389ddf64d169f199bb91e8b61fc1d2ee2522de
Norton Allen <allen@huarp.harvard.edu>
2012-10-30 19:03:10 +0000

    Uncompiled partial implementation of NetCDF initialization

--- BAT/BS2cdf.cc
+++ BAT/BS2cdf.cc
@@ -4,11 +4,93 @@
 #include "BS2cdf.h"
 
 const char *data_path;
+const char *setup_path;
+
+/** Parses configuration line to initialize member fields.
+    In the event of a syntax error, valid will be false.
+    If device is -99, valid will be false and device will be
+    -99, but no other fields will be initialized.
+*/
+BS2Cchan::BS2Cchan(const char *line, const char *filename, int ln) {
+  int i, n, nc;
+  device = 0;
+  valid = false;
+  cfg = line;
+  file = filename;
+  line_num = ln;
+  cp = 0;
+  
+  if (parse_str(label, sizeof(label)), "label") return;
+  n = sscanf(&cfg[cp], "%hd%hd%hd%n", &device, &phsicalChannel, &frequency, &nc);
+  if (n == 1 && device == -99) return;
+  else if (chk_sscanf(n != 2, nc, "device, channel or frequency"))
+    return;
+  if (parse_str(units, sizeof(units)), "units") return;
+  n = sscanf(&cfg[cp], "%hd%n", &cal_pwr, &nc);
+  if (chk_sscanf(n != 2, nc, "cal_pwr") return;
+  if (cal_pwr > 5) {
+    nl_error(2, "%s:%d Invalid cal_pwr: %d", file, line_num, cal_pwr);
+    return;
+  }
+  for (i = 0; i < cal_pwr; ++i) {
+    n = sscanf(&cfg[cp], "%f%n", &coef[i], &nc);
+    if (chk_sscanf(n != 1, nc, "coef")) return;
+  }
+  for (; i < 5; ++i) coef[i] = 0.;
+  n = sscanf(&cfg[cp], "%f%f%n", &min, &max, &nc);
+  if (chk_sscanf(n != 2, nc, "min or max")) return;
+  if (parse_str(cFormat, sizeof(cFormat)), "cFormat") return;
+  if ((strcasecmp(cFormat, "NC_CHAR")) &&
+      (strcasecmp(cFormat, "NC_SHORT")) &&
+      (strcasecmp(cFormat, "NC_INT")))
+    nl_error(2, "%s:%d Invalid cFormat for %s: '%s'", file, line_num, label, cFormat
+  n = sscanf(&cfg[cp], "%f%f%n", &scaleFactor, &addOffset, &nc);
+  if (chk_sscanf(n != 2, nc, "scaleFactor or addOffset"))
+    return;
+  if (parse_str(longName, sizeof(longName)), "longName") return;
+  valid = true;
+}
+
+BS2Cchan::~BS2Cchan() {}
+
+int BS2Cchan::chk_sscanf(int bad_ret, int nc, const char *what) {
+  if (bad_ret) {
+    nl_error(2, "%s:%d Syntax error reading %s for channel %s",
+      file, line_num, what, label);
+    return 1;
+  }
+  cp += nc;
+  return 0;
+}
+
+/** @return non-zero if no string is found or string is more than len-1 characters.
+ */
+int BS2Cchan::parse_str(char *ibuf, int len, const char *strname, int ws_ok) {
+  int n = 0;
+  while (cfg[cp] && isspace(cfg[cp])) ++cp;
+  while (cfg[cp] && (ws_ok || !isspace(cfg[cp]))) {
+    if (n+1 >= len) {
+      nl_error(2, "%s:%d String overflow parsing %s", file, line_num, strname);
+      return 1;
+    }
+    ibuf[n++] = cfg[cp];
+  }
+  // Trim trailing whitespace
+  while (ws_ok && n > 0 && isspace(ibuf[n-1]))
+    --n;
+  ibuf[n] = '\0';
+  return n > 0;
+}
 
 unsigned char BS2cdf::ibuf[BS2cdf::n_rec][BS2cdf::nb_rec];
 
 BS2cdf::BS2cdf() {
   ncid = -1;
+  opened = false;
+}
+
+BS2cdf::~BS2cdf() {
+  nc_close();
 }
 
 const char *BS2cdf::back_tick(const char *fmt, ...) {
@@ -57,10 +139,102 @@ mlf_def_t *BS2cdf::mlf_init(const char *data_path) {
   return mlf;
 }
 
-void BS2cdf::nc_setup() {
+void BS2cdf::nc_setup(const char *data_path, const char *setup_path) {
+  int nc_err, nc;
+  char setup[160];
+  FILE *ifp;
+  char title[100], date[20];
+  char chan_def[250];
+  struct tm *d;
+  time_t now;
+
+  nc_err = nc_create("BAT_SPAN.ncr", NC_CLOBBER, &ncid);
+  if (nc_err != NC_NOERR)
+    nl_error(3, "Unable to create BAT_SPAN.ncr");
+  opened = true;
+  nc_err = nc_def_dim(ncid, "scan", NC_UNLIMITED, &dimids[0]));
+  if (nc_err != NC_NOERR)
+    nl_error(3, "Error creating unlimited scan dimension");
+  
+  if (setup_path == NULL) {
+    if (data_path == NULL) data_path = ".";
+    snprintf(setup, 160, "%s/BAT_setup.txt", data_path);
+    setup_path = &setup[0];
+  }
+  ifp = fopen(setup_path, "r");
+  if (ifp == NULL)
+    nl_error(3, "Unable to read setup file: '%s'", setup_path);
+  line_num = 1;
+  if (fgets(title, sizeof(title), ifp) == NULL)
+    nl_error(3, "%s:%d Unexpected EOF reading title", setup_path, line_num);
+  nc = strlen(title);
+  while (nc > 0 && isspace(title[nc-1])
+    title[--nc] = '\0';
+  nc_err = nc_put_att_text(ncid, NC_GLOBAL, "title", nc, title))
+  if (nc_err != NC_NOERR)
+    nl_error(3, "Error setting title");
+
+  now = time(NULL);
+  d = localtime(&now);
+  snprintf(date, sizeof(date), "%02d/%02d/%04d", d->tm_mon+1, d->tm_mday, d->tm_year+1900);
+  nc_err = nc_put_att_text(ncid, NC_GLOBAL, "date", strlen(date), date);
+  if (nc_err != NC_NOERR)
+    nl_error(3, "Error setting date");
+
+  /* Swallow device definitions */
+  for(;;) {
+    ++line_num;
+    if (fgets(chan_def, sizeof(chan_def), ifp) == NULL)
+      nl_error(3, "%s:%d Unexpected end of file in setup file", setup_path, line_num);
+    if (strlen(chan_def)+1 >= sizeof(chan_def))
+      nl_error(3, "%s:%d Input line too long", setup_path, line_num);
+    if(atoi(chan_def) == -99) break;
+  }
+  
+  /* Now read channel definitions */
+  for(;;) {
+    BS2Cchan *BC;
+    ++line_num;
+    if (fgets(chan_def, sizeof(chan_def), ifp) == NULL)
+      nl_error(3, "%s:%d Unexpected end of file in setup file", setup_path, line_num);
+    if (strlen(chan_def)+1 >= sizeof(chan_def))
+      nl_error(3, "%s:%d Input line too long in setup file", setup_path, line_num);
+    BC = new BS2Cchan(chan_def, setup_path, line_num);
+    if (BC->valid) {
+      chan.push_back(BC);
+      // Add definitions to ncid
+      /*  setup_template.txt variable columns:
+          label  string (var name)
+          device  short integer
+          physicalChannel short integer
+          frequency short integer
+          units string
+          cal_pwr short integer (number of coef for polynomial fit)
+          coef[0 .. cal_pwr-1] float
+          min float min expected engineering value (should actually be min expected raw value)
+          max float max expected engineering value (")
+          cFormat string netcdf data type
+          scaleFactor float
+          addOffset float
+          longName string */
+    } else {
+      bool done = (BC->device == -99);
+      delete BC;
+      if (done) break;
+    }
+  }
+  
+  fclose(ifp);
+  if (nc_enddef(ncid) != NC_NOERR)
+    nl_error(3, "Error leaving define mode");
 }
 
 void BS2cdf::nc_close() {
+  if (opened) {
+    if (::nc_close(ncid) != NC_NOERR)
+      nl_error(2, "Error closing NetCDF file");
+    opened = false;
+  }
 }
 
 void BS2cdf::Parse_Record(unsigned char *rec) {
--- BAT/BS2cdf.h
+++ BAT/BS2cdf.h
@@ -7,12 +7,43 @@
 #include "mlf.h"
 
 extern const char *data_path;
+extern const char *setup_path;
 
 #ifdef __cplusplus
+
+#include <dequeue>
+
+class BS2Cchan {
+  public:
+    BS2Cchan(const char *line, const char *filename, int line_num);
+    ~BS2Cchan();
+    bool valid;
+    char label[12];
+    short device
+    short phsicalChannel;
+    short frequency;
+    char units[12];
+    short cal_pwr;
+    float coef[5];
+    float min;
+    float max;
+    char cFormat[9];
+    float scaleFactor;
+    float addOffset;
+    char longName[100];
+  private:
+    int parse_str(char *ibuf, int len, const char *strname, int ws_ok = 0);
+    int chk_sscanf(int bad_ret, int nc, const char *what);
+    const char *cfg;
+    const char *file;
+    unsigned line_num;
+    int cp;
+};
 
 class BS2cdf {
   public:
     BS2cdf();
+    ~BS2cdf();
     mlf_def_t *mlf_init(const char *data_path);
     void nc_setup();
     int Read_File(FILE *fp);
@@ -21,12 +52,15 @@ class BS2cdf {
   private:
     void Parse_Record(unsigned char *rec);
     const char *back_tick(const char *fmt, ...);
+    bool opened;
+    int ncid, dimids[2];
+    unsigned line_num;
+    std::vector<BS2Cchan *> chan;
     static const int SPAN_nb_rec = 104;
     static const int BAT_nb_rec = 35;
     static const int nb_rec = SPAN_nb_rec + BAT_nb_rec;
     static const int n_rec = 500; // defined in BSlogger.cc
     static unsigned char ibuf[n_rec][nb_rec];
-    int ncid;
 };
 
 #endif 
