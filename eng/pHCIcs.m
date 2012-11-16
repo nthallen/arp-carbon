ba466a002b1384385496d40309e016eae9edaf9a
Norton Allen <allen@huarp.harvard.edu>
2012-11-16 17:18:38 +0000

    New status bits

--- eng/pHCIcs.m
+++ eng/pHCIcs.m
@@ -2,4 +2,5 @@ function pHCIcs(varargin);
 % pHCIcs( [...] );
 % Coolant Status
 h = ne_dstat({
-  'CoolPump_DS', 'DS848', 4 }, 'Status', varargin{:} );
+  'CoolPump_DS', 'DS851', 7; ...
+	'CoolDivert_DS', 'DS848', 4 }, 'Status', varargin{:} );
