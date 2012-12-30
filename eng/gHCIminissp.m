a1db7424d52c4bca3184b173119419ee28d3f86c
Norton Allen <allen@huarp.harvard.edu>
2012-12-30 17:36:30 +0000

    Some rearrangement

--- eng/gHCIminissp.m
+++ eng/gHCIminissp.m
@@ -1,5 +1,5 @@
 function fig = gHCIminissp(varargin);
 % gHCIminissp(...)
 % MINI_SSP
-ffig = ne_group(varargin,'MINI_SSP','pHCIminisspt','pHCIminisspf','pHCIminisspo','pHCIminissps','pHCIminisspskipped','pHCIminisspstale','pHCIminisspstatus');
+ffig = ne_group(varargin,'MINI_SSP','pHCIminisspo','pHCIminisspf','pHCIminissps','pHCIminisspskipped','pHCIminisspw','pHCIminisspstatus');
 if nargout > 0 fig = ffig; end
--- eng/gHCIminit.m
+++ eng/gHCIminit.m
@@ -1,5 +1,5 @@
 function fig = gHCIminit(varargin);
 % gHCIminit(...)
 % MINI_ Temps
-ffig = ne_group(varargin,'MINI_ Temps','pHCIminitr','pHCIminits','pHCIminitskin');
+ffig = ne_group(varargin,'MINI_ Temps','pHCIminitsspt','pHCIminitr','pHCIminits','pHCIminitskin');
 if nargout > 0 fig = ffig; end
