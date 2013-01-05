f84e25fa8aaf3cff3306e930c2b89be7aba9b83f
Norton Allen <allen@huarp.harvard.edu>
2013-01-05 17:46:27 +0000

    Find QCLI_*.m without load_waves()

---
+++ eng/cust_pHCIhkhksws.m
@@ -0,0 +1,19 @@
+function cust_pHCIhkhksws(h)
+% cust_pHCIhkhksws(h)
+% Customize plot created by pHCIhkhksws
+
+% pHCIhkhksws's definition:
+
+% function pHCIhkhksws(varargin);
+% % pHCIhkhksws( [...] );
+% % HK HK SW Stat
+% h = timeplot({'SWStat'}, ...
+%       'HK HK SW Stat', ...
+%       'SW Stat', ...
+%       {'SWStat'}, ...
+%       varargin{:} );
+
+% Example customizations include:
+set(h,'LineStyle','none','Marker','.');
+%   ax = get(h(1),'parent');
+%   set(ax,'ylim',[0 800]);
