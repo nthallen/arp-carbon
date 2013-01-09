46e49f79fbe36f9c89d4a57ed42f32c1e9f29ce5
David Sayres <sayres@huarp.harvard.edu>
2013-01-09 16:10:51 +0000

    added setpoint reset and changed name conventions of commands for consistancy.

--- TM/dccc.cmd
+++ TM/dccc.cmd
@@ -24,12 +24,12 @@
   ;
 &dccc_off_on <int>
   : CO2 Cell Htr Ctl { $0 = 27; }
-  : CO2 Laser PV Htr Ctl { $0 = 26; }
+  : CO2 LPV Htr Ctl { $0 = 26; }
   : CO2 QCLI Reset { $0 = 25; }
   : CO2 Scroll Pump { $0 = 1; }
   : CO2 Laser TEC { $0 = 28; }
   : ISO Cell Htr Ctl { $0 = 3; }
-  : ISO Detector PV Htr Ctl { $0 = 4; }
+  : ISO DPV Htr Ctl { $0 = 4; }
   : ISO LPV Ext TEC { $0 = 5; }
   : ISO QCLI Reset { $0 = 32; }
   : ISO Scroll Pump { $0 = 2; }
@@ -37,13 +37,13 @@
   : Coolant Pump { $0 = 7; }
   : Inverter Arm { $0 = 8; }
   : MINI Cell Htr Ctl { $0 = 42; }
-  : MINI Detector PV Htr Ctl { $0 = 43; }
-  : MINI LPV TEC { $0 = 45; }
+  : MINI DPV Htr Ctl { $0 = 43; }
+  : MINI LPV Ext TEC { $0 = 45; }
   : MINI QCLI Reset { $0 = 41; }
   : MINI Scroll Pump { $0 = 0; }
   : MINI Laser TEC { $0 = 44; }
-  : Sample Htr Ctl { $0 = 37; }
-  : Space Heater Controller { $0 = 38; }
+  : HK Sample Htr Ctl { $0 = 37; }
+  : HK Space Htr Ctl { $0 = 38; }
   : Gas Deck Flow Controller Closed { $0 = 40; }
 # : Gas Deck Flow Controller Open { $0 = 39; }
   ;
