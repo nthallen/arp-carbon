c350673cfc99310b8a9b7f106890a06e20c9ea90
Norton Allen <allen@huarp.harvard.edu>
2013-02-03 19:05:10 +0000

    Remove unnecessary stuff.

--- eng/cust_pHCIah.m
+++ eng/cust_pHCIah.m
@@ -17,24 +17,9 @@ function cust_pHCIah(h)
 %   set(h,'LineStyle','none','Marker','.');
 %   ax = get(h(1),'parent');
 %   set(ax,'ylim',[0 800]);
-xdata = get(h,'xdata');
-ydata = get(h,'ydata');
-vstale = ydata >= 655.355;
-ydata(vstale) = ydata(vstale) - 655.36;
-vbad = ydata >= 327.675;
-ydata(vbad) = ydata(vbad) - 327.68;
-vstale = vstale & ~vbad;
 ax = get(h(1),'parent');
-if any(vstale)
-    line(xdata(vstale), ydata(vstale), 'Parent', ax, 'LineStyle','none','Marker','.','Color',[0 1 0]);
-end
-if any(vbad)
-    line(xdata(vbad), ydata(vbad), 'Parent', ax, 'LineStyle','none','Marker','*','Color',[1 0 0]);
-end
-ydata(vbad | vstale) = NaN;
-set(h,'ydata',ydata);
 yl = get(ax,'YLim');
-nyl = [ min(yl(1), 0) max(yl(2), 20) ];
+nyl = [ min(yl(1), 0) max(yl(2), 10) ];
 if any(nyl ~= yl)
     set(ax,'YLim',nyl);
 end
