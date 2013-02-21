3f8e8adc353ce029c553bfe34ddcad38b1143b95
Norton Allen <allen@huarp.harvard.edu>
2013-02-21 21:33:44 +0000

    BS_recovery

--- BAT/BS_recovery.cc
+++ BAT/BS_recovery.cc
@@ -8,28 +8,46 @@
      starting offset
      msecs
 */
+#include <string.h>
 #include "BAT_SPAN_int.h"
+#include "nortlib.h"
+
+int find_string(const char *src, const char *pat, int n) {
+  int i;
+  for (i = 0; i < n - (int)strlen(pat); ++i) {
+    const char *s = src+i;
+    const char *p = pat;
+    while (*p != '\0' && *p == *s) {
+      ++p;
+      ++s;
+    }
+    if (*p == '\0') return i;
+  }
+  return -1;
+}
 
 int main(int argc, char **argv) {
   FILE *ifp;
   unsigned disk_block_num = 0;
   unsigned bat_span_block_num = 0;
   if (argc > 1) {
-    ifp = fopen(argv[1], "r");
+    ifp = fopen64(argv[1], "r");
     if (ifp == 0)
       nl_error(3, "Unable to open input file %s", argv[1]);
   } else ifp = stdin;
   for (disk_block_num = 0;; ++disk_block_num) {
-    unsigned char block[512], *pkt;
+    char block[512];
+    int offset;
+    const char *pat = "\xAA\x44\x13\x58\xFC\x01";
     int nb = fread(block, 1, 512, ifp);
     if (nb < 512) break;
-    pkt = strstr(block, "\xAA\x44\x13\x58\x1C")
-    if (pkt) {
-      struct INSPVASB *PVA = pkt;
-      if (PVA.GPS_msecs > 394000000L && PVA.GPS_msecs < 410000000L) {
-        unsigned offset = pkt - block;
-        printf("%5u %7u %3u %9u\n", ++bat_span_block_num, disk_block_num,
-          offset, PVA.GPS_msecs);
+    offset = find_string(block, pat, 512);
+    if (offset >= 0) {
+      struct INSPVASB *PVA = (struct INSPVASB *)(&block[offset]);
+      if (PVA->GPS_msecs > 394000000L && PVA->GPS_msecs < 410000000L) {
+      // if (PVA->GPS_msecs > 222000000L && PVA->GPS_msecs < 227000000L) {
+        printf("%5u %7u %3d %9ld\n", ++bat_span_block_num, disk_block_num,
+          offset, PVA->GPS_msecs);
       }
     }
   }
