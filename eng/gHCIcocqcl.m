0bf536ca52ce30b48b0b95600b231e2c648ea5e8
Norton Allen <allen@huarp.harvard.edu>
2013-04-17 17:19:35 +0000

    Eliminate unused files

--- eng/gHCIcocqcl.m
+++
@@ -1,5 +0,0 @@
-function fig = gHCIcocqcl(varargin);
-% gHCIcocqcl(...)
-% CO2 CQCL
-ffig = ne_group(varargin,'CO2 CQCL','pHCIcocqclp','pHCIcocqclt','pHCIcocqclrh');
-if nargout > 0 fig = ffig; end
--- eng/gHCIminimqcl.m
+++
@@ -1,5 +0,0 @@
-function fig = gHCIminimqcl(varargin);
-% gHCIminimqcl(...)
-% MINI MQCL
-ffig = ne_group(varargin,'MINI MQCL','pHCIminimqclp','pHCIminimqclt','pHCIminimqclrh');
-if nargout > 0 fig = ffig; end
